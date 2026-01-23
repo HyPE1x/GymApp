83fbaeac479bec675792b0a4fae0f3df09184f8b
HyPE1x <ericgordeyev@gmail.com>
2026-01-22 22:01:52 -0800

    CORS change again

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ app.use(cors({
   origin: allowedOrigins,
   credentials: true,
   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization"],
 }));
 
 app.use(express.json());
